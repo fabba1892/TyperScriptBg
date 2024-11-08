69eb09bdc64cc9b00c6a619c9b218c334ccf154a
fabba1892 <Fabian.vanwyk@gmail.com>
2024-11-08 20:19:39 +0200

    strings numbers array

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let greeting = "Hi from CapeTeewn!🚠";
-console.log(greeting);
+console.log("Hi from CapeTeewn!🚠");
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-let greeting: string = "Hi from CapeTeewn!🚠";
-console.log(greeting);
-export { };
+// enum weekDay {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY};
+enum weekDay {
+    MONDAY = "Monday",
+    TUESDAY = "Tuesday",
+    WEDNESDAY = "Wednesday",
+    THURSDAY = "Thursday",
+    FRIDAY = "Friday"
+};
+
+const employee: {
+    empName: string,
+    dependents: number,
+    committees: [string, boolean],
+    payDay: string;
+} = {
+    empName: "Luca",
+    dependents: 2,
+    committees: ["Philantrophy", true],
+    payDay: weekDay.FRIDAY
+};
+
+employee.committees.push("fabba1892, 200");
+console.log(employee);
